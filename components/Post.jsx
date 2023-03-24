597509e3a68561b6d3fe5c4ce85b613fb8a701ee
Subham Mishra <63531369+msubham193@users.noreply.github.com>
2023-03-25 00:57:10 +0530

    dnhd

---
+++ components/Post.jsx
@@ -0,0 +1,125 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+// import Moment from "react-moment";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+const Post = () => {
+  const [likes, setLikes] = useState([]);
+  const [comments, setComments] = useState([]);
+  const router = useRouter();
+
+  const deletePost = ()=>{}
+  return (
+    <div className="flex p-3 cursor-pointer border-b border-gray-200">
+      {/* user image */}
+      <img
+        className="h-11 w-11 rounded-full mr-4"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLe5PABjXc17cjIMOibECLM7ppDwMmiDg6Dw&usqp=CAU"
+        alt="user-img"
+      />
+      {/* right side */}
+      <div className="flex-1">
+        {/* Header */}
+
+        <div className="flex items-center justify-between">
+          {/* post user info */}
+          <div className="flex items-center space-x-1 whitespace-nowrap">
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+               Subham Mishra
+            </h4>
+            <span className="text-sm sm:text-[15px]">
+              @msubham193
+            </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+        
+            </span>
+          </div>
+
+          {/* dot icon */}
+          <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
+        </div>
+
+        {/* post text */}
+
+        <p
+          onClick={() => router.push(`/posts/${id}`)}
+          className="text-gray-800 text-[15px sm:text-[16px] mb-2"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ut.
+        </p>
+
+        {/* post image */}
+
+        <img
+          onClick={() => router.push(`/posts/${id}`)}
+          className="rounded-2xl mr-2"
+          src="https://images.unsplash.com/photo-1679597454493-d86b77bdf2fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2NXx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60"
+          alt=""
+        />
+
+        {/* icons */}
+
+        <div className="flex justify-between text-gray-500 p-2">
+          <div className="flex items-center select-none">
+            <ChatIcon
+            //   onClick={() => {
+            //     if (!currentUser) {
+            //       // signIn();
+            //       router.push("/auth/signin");
+            //     } else {
+            //       setPostId(id);
+            //       setOpen(!open);
+            //     }
+            //   }}
+              className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
+            />
+            {comments.length > 0 && (
+              <span className="text-sm">{comments.length}</span>
+            )}
+          </div>
+          {true&& (
+            <TrashIcon
+              onClick={deletePost}
+              className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+            />
+          )}
+          <div className="flex items-center">
+            {true ? (
+              <HeartIconFilled
+                // onClick={likePost}
+                className="h-9 w-9 hoverEffect p-2 text-red-600 hover:bg-red-100"
+              />
+            ) : (
+              <HeartIcon
+                // onClick={likePost}
+                className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+              />
+            )}
+            {likes.length > 0 && (
+              <span
+                className={`${true&& "text-red-600"} text-sm select-none`}
+              >
+                {" "}
+                {likes.length}
+              </span>
+            )}
+          </div>
+
+          <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
