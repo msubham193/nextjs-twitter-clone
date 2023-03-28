5eb3639d9464c27a93bbaf8858307f7eb1155a04
Subham Mishra <63531369+msubham193@users.noreply.github.com>
2023-03-28 19:52:29 +0530

    widget section done

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,58 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Feed from '../components/Feed'
-import Sidebar from '../components/Sidebar'
+import Head from "next/head";
+import Image from "next/image";
+import Feed from "../components/Feed";
+import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
 
-
-export default function Home() {
+export default function Home({ newsResults, randomUsersResults }) {
   return (
-    <div >
+    <div>
       <Head>
         <title>twitter-clone</title>
         <meta name="description" content="a nextjs twitter clone" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <Sidebar/>
-
-     <Feed/>
-
-    
+      <main className="flex min-h-screen mx-auto">
+        {" "}
+        <Sidebar />
+        <Feed />
+        <Widgets
+          newsResults={newsResults?.articles}
+          randomUsersResults={randomUsersResults?.results || null}
+        />
+      </main>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const newsResults = await fetch(
+    "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
+  ).then((res) => res.json());
+
+  // Who to follow section
+
+  let randomUsersResults = [];
+
+  try {
+    const res = await fetch(
+      "https://randomuser.me/api/?results=30&inc=name,login,picture"
+    );
+
+    randomUsersResults = await res.json();
+  } catch (e) {
+    randomUsersResults = [];
+  }
+
+  // const randomUsersResults = await fetch(
+  //   "https://randomuser.me/api/?results=30&inc=name,login,picture"
+  // ).then((res) => res.json());
+
+  return {
+    props: {
+      newsResults,
+      randomUsersResults,
+    },
+  };
 }
